e52e4411f05aa7c0c30103221fb397db21319edd
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2024-04-21 20:03:15 -0300

    integração

--- src/pages/registerPage/formContent.jsx
+++ src/pages/registerPage/formContent.jsx
@@ -1,36 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Form, FormGroup, FormInput, Container, Button } from "semantic-ui-react"
 import InputMask from "react-input-mask";
 
 export default function FormContent() {
-    return(
-        <div style={{marginTop: '25%'}}>
+
+    const [nomeCompleto, setNomeCompleto] = useState();
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+    const [cpf, setCpf] = useState();
+    const [numeroTelefone, setNumeroTelefone] = useState();
+
+    function salvar() {
+
+		let usuarioRequest = {
+            nomeCompleto: nomeCompleto,
+		     email: email,
+		     senha: senha,
+		     cpf: cpf,
+		     numeroTelefone: numeroTelefone
+		}
+	
+		axios.post("http://localhost:8080/api/usuario", usuarioRequest)
+		.then((response) => {
+		     console.log('Usuario cadastrado com sucesso.')
+		})
+		.catch((error) => {
+		     console.log('Erro ao incluir o um Usuario.')
+		})
+	}
+
+
+
+    return (
+        <div style={{ marginTop: '25%' }}>
             <Container textAlign="center">
                 <h1>Crie a Sua Conta</h1>
-                <div style={{marginTop: '5%'}}>
+                <div style={{ marginTop: '5%' }}>
                     <Form>
                         <FormGroup widths='equal'>
-                            <FormInput 
+                            <FormInput
                                 required
                                 fluid
                                 label='Nome Completo'
                                 placeholder='nome completo'
+                                value={nomeCompleto}
+                                onchange={e => setNomeCompleto(e.target.value)}
                             />
-                            <FormInput 
+                            <FormInput
                                 required
                                 fluid
                                 label='Email'
                                 placeholder='email'
+                                value={email}
+                                onchange={e => setEmail(e.target.value)}
                             />
                         </FormGroup>
                         <FormGroup widths='equal'>
-                            <FormInput 
+                            <FormInput
                                 required
                                 fluid
                                 label='Senha'
                                 placeholder='senha'
+                                value={senha}
+                                onchange={e => setSenha(e.target.value)}
                             />
-                            <FormInput 
+                            <FormInput
                                 required
                                 fluid
                                 label='Confirmar Senha'
@@ -38,27 +73,32 @@ export default function FormContent() {
                             />
                         </FormGroup>
                         <FormGroup widths='equal'>
-                            <FormInput 
+                            <FormInput
                                 required
                                 fluid
                                 label='Telefone'>
-                                <InputMask 
+                                <InputMask
                                     mask="(99) 99999-9999"
+                                    value={numeroTelefone}
+                                    onchange={e => setNumeroTelefone(e.target.value)}
                                 />
                             </FormInput>
-                            <FormInput 
+                            <FormInput
                                 required
                                 fluid
                                 label='CPF'>
-                                <InputMask 
+                                <InputMask
                                     mask="999.999.999-99"
+                                    value={cpf}
+                                    onchange={e => setCpf(e.target.value)}
                                 />
                             </FormInput>
                         </FormGroup>
                     </Form>
                 </div>
 
-                <Button color="orange" circular size="large" style={{color: 'black'}}>Cadastrar</Button>
+                <Button color="orange" circular size="large" style={{ color: 'black' }} onClick={() => salvar()} 
+                >Cadastrar</Button>
             </Container>
         </div>
     )
