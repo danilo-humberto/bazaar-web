28d234fe9708fcb5f5cc0e9855c92b996a51d5cd
danilohumberto” <danilohumberto12@hotmail.com>
2024-08-05 09:17:12 -0300

    add: adicionando tela de perfil

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -5,6 +5,7 @@ import RegisterPage from "./pages/registerPage/registerPage";
 import LoginPage from "./pages/loginPage/LoginPage";
 import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
 import MainPage from "./pages/mainPage/MainPage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 function Rotas() {
   return (
@@ -13,6 +14,7 @@ function Rotas() {
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="forgotPassword" element={<ForgotPassword />} />
+        <Route path="profile" element={<ProfilePage />} />
         <Route path="/" element={ <MainPage />}/>
       </Routes>
     </>
---
+++ src/pages/profilePage/ProfilePage.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  Button,
+  Grid,
+  Image,
+  GridColumn,
+  ButtonGroup
+} from "semantic-ui-react";
+import Header from "../../components/header/header";
+import Footer from "../../components/otherFooter/otherFooter";
+
+import "./ProfilePage.css";
+
+/* 
+O primeiro GridColumn ainda precisar ser integrado com o banco, então o código nem é esse, tem que fazer ele buscando direto no banco.
+*/
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <Header />
+      <div className="container-profile">
+        <div className="background-profile">
+          <div className="content-profile">
+            <div className="grid-content-profile">
+              <Grid columns={2}>
+                <GridColumn width={4}>
+                  <Image
+                    src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+                    size="medium"
+                  />
+                  <p className="name">FULANO DE SOUZA</p>
+                  <p className="infos">
+                    Cep: 55555-55
+                    <br />
+                    (81) 9 9999-9999
+                    <br />
+                    fulano@gmail.com
+                  </p>
+                </GridColumn>
+
+                <GridColumn center>
+                  <ButtonGroup vertical>
+                    <Button
+                      color="orange"
+                      circular
+                      size="big"
+                    >
+                      Editar Perfil
+                    </Button>
+                    <br />
+                    <br />
+                    <Button color="orange" circular size="big">
+                      Produtos
+                    </Button>
+                    <br />
+                    <br />
+                    <Button color="orange" circular size="big">
+                      Adicionar Endereço
+                    </Button>
+                    <br />
+                    <br />
+                  </ButtonGroup>
+                </GridColumn>
+              </Grid>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
