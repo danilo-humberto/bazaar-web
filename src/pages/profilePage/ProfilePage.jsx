4af973c6e111edb649be974826f00d06b5d8a756
aluno <aluno@ifpe.edu.br>
2024-08-07 15:33:00 -0300

    fazendo integração da tela de perfil

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -4,18 +4,62 @@ import {
   Grid,
   Image,
   GridColumn,
-  ButtonGroup
+  ButtonGroup,
 } from "semantic-ui-react";
 import Header from "../../components/header/header";
 import Footer from "../../components/otherFooter/otherFooter";
 
 import "./ProfilePage.css";
 
-/* 
-O primeiro GridColumn ainda precisar ser integrado com o banco, então o código nem é esse, tem que fazer ele buscando direto no banco.
-*/
+const getUserId = () => {
+  const userId = localStorage.getItem("userId");
+  console.log("Obtained userId:", userId);
+  return userId;
+};
+
+const fetchUserData = async (setUserData) => {
+  const userId = getUserId();
+
+  if (!userId) {
+    console.error("ID do usuário não encontrado");
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/usuario/${userId}`,
+      {
+        method: "GET",
+      }
+    );
+
+    console.log("Response status:", response.status);
+    if (response.ok) {
+      const userData = await response.json();
+      console.log("User data fetched successfully:", userData);
+      setUserData(userData);
+    } else {
+      console.error(
+        "Erro ao buscar dados do usuário",
+        response.status,
+        response.statusText
+      );
+    }
+  } catch (error) {
+    console.error("Erro ao fazer a requisição:", error);
+  }
+};
 
 export default function ProfilePage() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const testUserId = "1";
+    localStorage.setItem("userId", testUserId);
+
+    fetchUserData(setUserData);
+  }, []);
+
   return (
     <div>
       <Header />
@@ -25,27 +69,34 @@ export default function ProfilePage() {
             <div className="grid-content-profile">
               <Grid columns={2}>
                 <GridColumn width={4}>
-                  <Image
-                    src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-                    size="medium"
-                  />
-                  <p className="name">FULANO DE SOUZA</p>
-                  <p className="infos">
-                    Cep: 55555-55
-                    <br />
-                    (81) 9 9999-9999
-                    <br />
-                    fulano@gmail.com
-                  </p>
+                  {userData && (
+                    <>
+                      <Image src={userData.imagemUrl} size="small" />
+                      <List>
+                        <List.Item>
+                          <List.Header className>Nome</List.Header>
+                          {userData.nomeCompleto}
+                        </List.Item>
+                        <List.Item>
+                          <List.Header>Número</List.Header>
+                          {userData.numeroTelefone}
+                        </List.Item>
+                        <List.Item>
+                          <List.Header>Email</List.Header>
+                          {userData.email}
+                        </List.Item>
+                        <List.Item>
+                          <List.Header>Endereço</List.Header>
+                          {userData.endereco}
+                        </List.Item>
+                      </List>
+                    </>
+                  )}
                 </GridColumn>
 
                 <GridColumn center>
                   <ButtonGroup vertical>
-                    <Button
-                      color="orange"
-                      circular
-                      size="big"
-                    >
+                    <Button color="orange" circular size="big">
                       Editar Perfil
                     </Button>
                     <br />
